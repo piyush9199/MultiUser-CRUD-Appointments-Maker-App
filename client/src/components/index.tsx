6147148770e073ec324b3fb71fc305015efc8e3d
piyush9199 <piyush9199@gmail.com>
2025-03-01 11:04:44 +0530

    sourcecodelinkadded

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -2,12 +2,15 @@
 import { Link } from "react-router-dom"
 
 
-export function ToDoIndex(){
-    return(
+export function ToDoIndex() {
+    return (
         <div className="container-fluid">
-            <div className="d-flex justify-content-center align-items-center" style={{height: '100vh'}}>
-                <Link to="/register" className="btn btn-dark mx-2">Register User</Link>
-                <Link to="/login" className="btn btn-warning">User Login</Link>
+            <div>
+                <div><a href="https://github.com/piyush9199/Personal-TaskTracker" target="_blank" className="btn btn-danger mt-4 code-github-link ">Source Code</a></div>
+                <div className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
+                    <Link to="/register" className="btn btn-dark mx-2">Register User</Link>
+                    <Link to="/login" className="btn btn-warning">User Login</Link>
+                </div>
             </div>
         </div>
     )
